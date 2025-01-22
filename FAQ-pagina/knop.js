ae157824f10133f91a8586eb703a0045fe80f057
LuukvdBilt <338255@student.mboutrecht.nl>
2025-01-22 11:56:26 +0100

    hele FAQ pagina

--- FAQ-pagina/knop.js
+++
@@ -1,5 +0,0 @@
-function submitText() {
-    const text = document.getElementById('expandableTextarea').value;
-    alert('Je hebt dit verstuurd: ' + text); // You can process this, like sending it to a server
-    document.getElementById('expandableTextarea').value = ''; // Clear the textarea after submission
-}
